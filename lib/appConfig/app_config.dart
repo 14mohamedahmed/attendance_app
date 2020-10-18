0e157a88a08ab823ec875aeacebd6d24fbf48123
14mohamed <mohamedssheko980@gmail.com>
2020-10-18 15:32:16 +0200

    app config file

--- lib/appConfig/app_config.dart
+++ lib/appConfig/app_config.dart
@@ -2,21 +2,24 @@ import 'package:flutter/material.dart';
 
 class AppConfig {
   static MediaQueryData _mediaQueryData;
+  static var _orientation;
   static double screenWidth;
   static double screenHeight;
-  static double blockSizeHorizontal;
-  static double blockSizeVertical;
   static double _safeAreaHorizontal;
   static double _safeAreaVertical;
   static double safeBlockHorizontal;
   static double safeBlockVertical;
-  // declare ant device size
+
   void init(BuildContext context) {
     _mediaQueryData = MediaQuery.of(context);
-    screenWidth = _mediaQueryData.size.width;
-    screenHeight = _mediaQueryData.size.height;
-    blockSizeHorizontal = screenWidth / 100;
-    blockSizeVertical = screenHeight / 100;
+    _orientation = MediaQuery.of(context).orientation;
+    if (_orientation == Orientation.portrait) {
+      screenWidth = _mediaQueryData.size.width;
+      screenHeight = _mediaQueryData.size.height;
+    } else {
+      screenWidth = _mediaQueryData.size.height;
+      screenHeight = _mediaQueryData.size.width;
+    }
     _safeAreaHorizontal =
         _mediaQueryData.padding.left + _mediaQueryData.padding.right;
     _safeAreaVertical =
